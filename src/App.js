bf8fcb977caf77318aa8d6768da93ff09b3b0492
ABHISHEK TIWARI (Frontend-Developer) <tiwariabhishek27611@gmail.com>
2022-08-24 18:35:02 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <>
+      <div className="main-box">
+        <div className="box">
+          <h1>GeeksforGeeks</h1>
+          <h2>Media Query</h2>
+        </div>
+        <div className="box">
+          <h1>GeeksforGeeks</h1>
+          <h2>Media Query</h2>
+        </div>
+        <div className="box">
+          <h1>GeeksforGeeks</h1>
+          <h2>Media Query</h2>
+        </div>
+        <div className="box">
+          <h1>GeeksforGeeks</h1>
+          <h2>Media Query</h2>
+        </div>
+        <div className="box">
+          <h1>GeeksforGeeks</h1>
+          <h2>Media Query</h2>
+        </div>
+      </div>
+    </>
+  );
+}
+
+// Mobile (Smartphone) max-width: 480px
+// Low Resolution Tablets and ipads max-width: 767px
+// Tablets Ipads portrait mode max-width:1024px
+// Desktops max-width:1280px
+// Huge size (Larger screen) max-width: 1281px and greater
